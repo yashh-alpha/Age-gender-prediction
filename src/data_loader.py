27c8e637e491594791c3c541e1728199c7cdec36
yashh-alpha <kyash4886@gmail.com>
2026-06-04 16:20:41 +0530

    adding model

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,23 +1,48 @@
 import os
+import cv2
+import numpy as np
 import pandas as pd
 
-def create_dataframe(dataset_path):
-
-    data = []
+def load_dataset(dataset_path):
+    images = []
+    ages = []
+    genders = []
 
     for file in os.listdir(dataset_path):
-
         try:
             age = int(file.split("_")[0])
             gender = int(file.split("_")[1])
 
-            data.append({
-                "image_path": os.path.join(dataset_path, file),
-                "age": age,
-                "gender": gender
-            })
+            img_path = os.path.join(
+                dataset_path,
+                file
+            )
+
+            img = cv2.imread(img_path)
+
+            if img is None:
+                continue
+            img = cv2.cvtColor(
+                img,
+                cv2.COLOR_BGR2RGB
+            )
+
+            img = cv2.resize(
+                img,
+                (224,224)
+            )
+
+            images.append(img)
+            ages.append(age)
+            genders.append(gender)
 
         except:
             pass
+    
+    X = np.array(images)/255.0
 
-    return pd.DataFrame(data)
+    return (
+        X,
+        np.array(ages),
+        np.array(genders)
+    )
--- src/train.py
+++ src/train.py
@@ -1,1 +1,25 @@
 from sklearn.model_selection import train_test_split
+from keras.callbacks import EarlyStopping, ModelCheckpoint,ReduceLROnPlateau
+from data_loader import load_dataset
+from model import Build_model
+
+X, y_age, y_gender = load_dataset('data\raw\UTKFace')
+
+X_train,X_val,age_train,age_val,gender_train,gender_val = train_test_split(X,y_age,y_gender,test_size=0.2,random_state=42)
+
+model = Build_model()
+
+model.compile(
+    optimizer='adam',
+    loss ={'age_output':'mae',
+           'gender_ouput':'binary_crossentropy'},
+    metrics = {'gender_ouput':['accuracy']}
+)
+
+model.fit(X_train,
+          {'age_output':age_train,'gender_output':gender_train},epochs = 10,
+          batch_size = 32,
+          validation_data=(X_val,{'age_output':age_val,'gender_output':gender_val})
+        )
+
+model.save("models/age_gender_model.keras")
